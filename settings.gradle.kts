97141f83a8c111afcc13db2b7e0c8aafd67615a0
Tyler Cook <tcc@sandpolis.com>
2021-02-11 17:53:51 -1000

    Add root settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+//============================================================================//
+//                                                                            //
+//                         Copyright © 2015 Sandpolis                         //
+//                                                                            //
+//  This source file is subject to the terms of the Mozilla Public License    //
+//  version 2. You may not use this file except in compliance with the MPL    //
+//  as published by the Mozilla Foundation.                                   //
+//                                                                            //
+//============================================================================//
+
+rootProject.name = "com.sandpolis.agent.vanilla"
+
+// Core modules
+include("module:com.sandpolis.core.agent")
+include("module:com.sandpolis.core.clientagent")
+include("module:com.sandpolis.core.foundation")
+include("module:com.sandpolis.core.instance")
+include("module:com.sandpolis.core.net")
+include("module:com.sandpolis.core.serveragent")
